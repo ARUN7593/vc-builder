ae49fd6cfebff10a05be0fb0d7b04f0838ec4764
arun7593 <arunmca7593@gmail.com>
2024-07-04 13:01:41 +0530

    vcbuilder project

---
+++ src/InteriorDesign.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import img1 from "./assets/vbimage1.png";
+
+const InteriorDesign = () => {
+  return (
+    <>
+      <div
+        className="border-2 border-blue md:mx-20 w-auto h-auto lg:grid lg:grid-cols-2 lg:grid-rows-3 
+      {md:grid md:grid-cols-2 md:grid-rows-3 max-md:w-full h-96}md   {sm:grid sm:grid-cols-2 sm:gird-rows-3 max-sm:w-full h-48 overflow:hidden}sm"
+      >
+        {/* <div className="border-2 border-blue mx-auto max-w-7xl grid gap-4 sm:grid-cols-2 lg:grid-cols-3"> */}
+        <div className="border-2 border-blue w-full h-96">
+          <img
+            src={img1}
+            alt="interiorimg1"
+            srcset=""
+            className="w-full h-96"
+          />
+        </div>
+        <div className="border-2 border-blue w-500px h-auto mx-10 my-10 text-black text-justify">
+          Space: The foundation of any interior design. It involves
+          understanding the physical space and how to use it effectively. Line:
+          Lines guide the eye and create shapes in a space. They can be
+          horizontal, vertical, or dynamic (like diagonal or curved lines).
+          Form: Forms are shapes with volume, such as furniture or decor. They
+          can be geometric or organic. Light: Essential for visibility and mood.
+          Natural and artificial lighting are both crucial. Color: Influences
+          mood and perception. Color schemes are carefully chosen to match the
+          purpose and style of the space. Texture: Adds depth and interest.
+          Space: The foundation of any interior design. It involves
+          understanding the physical space and how to use it effectively. Line:
+          Lines guide the eye and create shapes in a space. They can be
+          horizontal, vertical, or dynamic (like diagonal or curved lines).
+          Form: Forms are shapes with volume, such as furniture or decor. They
+          can be geometric or organic. Light: Essential for visibility and mood.
+          Natural and artificial lighting are both crucial. Color: Influences
+          mood and perception. Color schemes are carefully chosen to match the
+          purpose and style of the space.
+        </div>
+        <div className="border-2 border-blue w-500px h-auto mx-10 my-10 text-black text-justify">
+          Space: The foundation of any interior design. It involves
+          understanding the physical space and how to use it effectively. Line:
+          Lines guide the eye and create shapes in a space. They can be
+          horizontal, vertical, or dynamic (like diagonal or curved lines).
+          Form: Forms are shapes with volume, such as furniture or decor. They
+          can be geometric or organic. Light: Essential for visibility and mood.
+          Natural and artificial lighting are both crucial. Color: Influences
+          mood and perception. Color schemes are carefully chosen to match the
+          purpose and style of the space. Texture: Adds depth and interest.
+          Space: The foundation of any interior design. It involves
+          understanding the physical space and how to use it effectively. Line:
+          Lines guide the eye and create shapes in a space. They can be
+          horizontal, vertical, or dynamic (like diagonal or curved lines).
+          Form: Forms are shapes with volume, such as furniture or decor. They
+          can be geometric or organic. Light: Essential for visibility and mood.
+          Natural and artificial lighting are both crucial. Color: Influences
+          mood and perception. Color schemes are carefully chosen to match the
+          purpose and style of the space.
+        </div>
+        <div className="border-2 border-blue w-full h-96">
+          <img
+            src={img1}
+            alt="interiorimg1"
+            srcset=""
+            className="w-full h-96"
+          />
+        </div>
+        <div className="border-2 border-blue w-full h-96">
+          <img
+            src={img1}
+            alt="interiorimg1"
+            srcset=""
+            className="w-full h-96"
+          />
+        </div>
+        <div className="border-2 border-blue w-500px h-auto mx-10 my-10 text-black text-justify">
+          Space: The foundation of any interior design. It involves
+          understanding the physical space and how to use it effectively. Line:
+          Lines guide the eye and create shapes in a space. They can be
+          horizontal, vertical, or dynamic (like diagonal or curved lines).
+          Form: Forms are shapes with volume, such as furniture or decor. They
+          can be geometric or organic. Light: Essential for visibility and mood.
+          Natural and artificial lighting are both crucial. Color: Influences
+          mood and perception. Color schemes are carefully chosen to match the
+          purpose and style of the space. Texture: Adds depth and interest.
+          Space: The foundation of any interior design. It involves
+          understanding the physical space and how to use it effectively. Line:
+          Lines guide the eye and create shapes in a space. They can be
+          horizontal, vertical, or dynamic (like diagonal or curved lines).
+          Form: Forms are shapes with volume, such as furniture or decor. They
+          can be geometric or organic. Light: Essential for visibility and mood.
+          Natural and artificial lighting are both crucial. Color: Influences
+          mood and perception. Color schemes are carefully chosen to match the
+          purpose and style of the space.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InteriorDesign;
